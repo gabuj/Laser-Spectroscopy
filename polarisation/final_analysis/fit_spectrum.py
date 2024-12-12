5f466c60484382930baf1b772ea4d29808822494
gabuj <gabujamesmarino@gmail.com>
2024-12-12 18:02:41 +0000

    last change before we go

--- polarisation/final_analysis/fit_spectrum.py
+++ polarisation/final_analysis/fit_spectrum.py
@@ -11,6 +11,30 @@ def spectrum(x, gamma_1, x0_1, gamma_2, x0_2, gamma_3, x0_3, gamma_4, x0_4, gamm
 
 I0_guess= -0.01
 
+# gamma1_guess= 1e7
+# x0_1_guess = 6.6e8
+# A1_guess = 10e-5
+
+# gamma2_guess = 1e7
+# x0_2_guess = 7.3e8
+# A2_guess = 18e-5
+
+# gamma3_guess = 1e7
+# x0_3_guess = 7.6e8
+# A3_guess =  14e-5
+
+# gamma4_guess = 1e7
+# x0_4_guess = 8.1e8
+# A4_guess =  13e-5
+
+# gamma5_guess = 1e7
+# x0_5_guess = 8.5e8
+# A5_guess = 6e-5
+
+# gamma6_guess = 1e7
+# x0_6_guess = 8.9e8
+# A6_guess = 5e-5
+
 gamma1_guess= 1e7
 x0_1_guess = 6.6e8
 A1_guess = 10e-5
@@ -21,11 +45,11 @@ A2_guess = 18e-5
 
 gamma3_guess = 1e7
 x0_3_guess = 7.6e8
-A3_guess =  4e-5
+A3_guess =  14e-5
 
 gamma4_guess = 1e7
 x0_4_guess = 8.1e8
-A4_guess =  3e-5
+A4_guess =  13e-5
 
 gamma5_guess = 1e7
 x0_5_guess = 8.5e8
@@ -34,6 +58,8 @@ A5_guess = 6e-5
 gamma6_guess = 1e7
 x0_6_guess = 8.9e8
 A6_guess = 5e-5
+
+
 initial_guesses = [gamma1_guess, x0_1_guess, gamma2_guess, x0_2_guess, gamma3_guess, x0_3_guess, gamma4_guess, x0_4_guess, gamma5_guess, x0_5_guess, gamma6_guess, x0_6_guess, I0_guess, A1_guess, A2_guess, A3_guess, A4_guess, A5_guess, A6_guess]
 
 def fit(f, intensities):
