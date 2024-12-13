daf7d08fee4502244232e5aea9bcc6302023ddec
gabuj <gabujamesmarino@gmailcom>
2024-12-13 14:41:22 +0000

    last data

--- polarisation/graphic_pol.py
+++ polarisation/graphic_pol.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-filename = 'polarisation/all_angles_rotating_linpol.txt'
+filename = 'polarisation/all_angles_rotating_linpol_20.txt'
 angle, power, power_err = np.loadtxt(filename, usecols=(0, 1,2), skiprows= 2, unpack=True)
 
 angle= angle*np.pi/180
